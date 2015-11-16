dc1f16b250473a6089e2cc68a684af24f2253646
string44 <jason.pitogo1@gmail.com>
2015-11-17 00:12:42 +0800

    NATIVE PHP WITH UI SA HOME

---
+++ pages/index.php
@@ -0,0 +1,409 @@
+<html>
+<head>
+    <title>Start&Boost</title>
+</head>
+<!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="../text/css" href="css/isotope.css" media="screen" />
+    <link rel="stylesheet" href="../js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../css/da-slider.css" />
+    <!-- Owl Carousel Assets -->
+    <link href="../js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles.css" />
+     <link rel="stylesheet" href="../css/AdminLTE.min.css" />
+    <!-- Font Awesome -->
+    <link href="font/css/font-awesome.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="../dist/css/skins/skin-blue.min.css">    
+<body>
+    <header class="header">
+        <div class="container">
+            <nav class="navbar-inverse" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand scroll-top logo"><img src='../images/SNBlogo.png' style="width:35%;"></a>
+                </div>
+                <!--/.navbar-header-->
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav" id="mainNav">
+                        <li class="active"><a href="#home" class="scroll-link">Home</a></li>
+                        <li><a href="#aboutUs" class="scroll-link">About Us</a></li>
+                        <!--<li><a href="#skills" class="scroll-link">Login</a></li>
+                        <li><a href="#experience" class="scroll-link">Register</a></li>
+                        <li><a href="#portfolio" class="scroll-link">Top 5 ideas</a></li>-->
+                        <li><a href="#contactUs" class="scroll-link">Contact Us</a></li>
+                    <!--   <li>
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="form-group">
+                              <input type="text" class="form-control" placeholder="Search">
+                            </div>
+                        </form>
+                        </li>
+                        <li>
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary form-control glyphicon glyphicon-search"></button>  &nbsp;&nbsp;                         
+                            </div>
+                        </form>
+                        </li>-->
+                    </ul>
+                </div>
+                <!--/.navbar-collapse-->
+            </nav>
+            <!--/.navbar-->
+        </div>
+        <!--/.container-->
+    </header>
+    <!--/.header-->
+    <div id="#top"></div>
+    <section id="home">
+        
+        <div class="banner-container">
+            <img src="../images/banner-bg2.jpg" alt="banner" />
+            <div class="container banner-content">
+                <div>
+                    <br/><br/><br/><br/><br/><br/>
+                    <div class="form-group col-md-3" style="float:right;border: 2px solid #D0D0D0;box-shadow: 0 0 8px #D0D0D0;">
+                        <br/><center><img src="../images/userlogin.png" style="width:30%;" /></center><br/>
+                        <!--<label for="name">Username</label>-->
+                      
+                        <input type="text" class="form-control" name="emailAdd" id="emailAdd" placeholder="username" title="Please enter your Username">
+                         
+                        <br/>
+                        <input type="password" class="form-control" name="pssWord" id="pssWord" placeholder="password" title="Please enter your password">  
+                         
+                        <br/>
+                        <button name="login" type="submit" class="btn btn-primary form-control" id="submit">Log-in</button>
+                        <br/><br/>
+                        <button name="create" type="button" class="btn btn-primary form-control" id="create" data-toggle="modal" data-target="#CreateAccount">Create Account</button>
+                        <br/>
+
+                        <div class="box-body">
+                   
+                        </div>
+                        <br/>
+                        
+                    </div>
+                    <!--<div class="form-group" style="float:right;">
+                        <label for="email">Password</label>
+                    </div>-->
+                </div>
+
+                <!--<div id="da-slider" class="da-slider">
+                    <div class="da-slide">
+                        <h2>Start in idea & boost it now!</h2>
+                        <p> -$index[5] </p>
+                        <div class="da-img"></div>
+                    </div>
+                    <div class="da-slide">
+                        <h2>We make imaginations into reality!!!</h2>
+                        <p>-$index[5]</p>
+                        <div class="da-img"></div>
+                    </div>
+                    <div class="da-slide">
+                        <h2>HTML5/CSS3</h2>
+                        <p>HTML5 is a markup language used for structuring and presenting Web.</p>
+                        <div class="da-img"></div>
+                    </div>
+                    <div class="da-slide">
+                        <h2>JavaScript/jQuery</h2>
+                        <p>jQuery: Write Less, Do More...</p>
+                        <div class="da-img"></div>
+                    </div>
+				  <nav class="da-arrows">
+                        <span class="da-arrows-prev"></span>
+                        <span class="da-arrows-next"></span>
+                    </nav> 
+                </div>-->
+            </div>
+        </div>
+    </section>
+    <section id="introText">
+        <div class="container">
+            <div class="text-center">
+            <h1>A Web and mobile-based startup ideas repository and exploration</h1>
+              <p><b>Start&Boost</b> is a Capstone project made by $index[5], a group of student from University of Cebu - Lapulapu and Mandaue. Anyone who has a brilliant idea is given an opportunity to share and discuss it with reliable members from different parts of the world, who's profile has reputation and badges. This system has three classifications of users: The ideators, investors, and company. Members can chat, create group, video conference, post ideas, upload product snapshots and links, and share to social medias.</p>
+            </div>
+        </div>
+
+    </section>
+    <!--About-->
+    <section id="aboutUs" class="secPad">
+        <div class="container">
+            <div class="heading text-center">
+                <!-- Heading -->
+                <h2>$index[5]</h2>
+                <p>MEET THE TEAM</p>
+            </div>
+            <div class="row">
+             
+              <!-- item -->
+                <div class="col-md-3 text-center tileBox">
+                   <div class="txtHead"><img src="../images/team/index0.png" style="width:40%;" /></i>
+                    <h3>Lyneth <span class="id-color">Cutamora</span></h3>
+                    <h6>$index[0]</h6></div>
+                    <p>Project Manager</p>
+                </div>
+                <!-- end: -->
+                  <!-- item -->
+                <div class="col-md-3 text-center tileBox">
+                   <div class="txtHead"><img src="../images/team/index1.jpg" style="width:40%;" />
+                    <h3>Jason <span class="id-color">Pitogo</span></h3>
+                    <h6>$index[1]</h6></div>
+                    <p>Software Engineer</p>
+                </div>
+                <!-- end: -->
+                <!-- item -->
+                <div class="col-md-3 text-center tileBox">
+                   <div class="txtHead"> <img src="../images/team/index2.jpg" style="width:40%;" />
+                    <h3>Alfie<span class="id-color">Dimpas</span></h3>
+                    <h6>$index[2]</h6></div>
+                    <p>Technical Writer</p>
+                </div>
+                <!-- end: -->
+             
+                <!-- item -->
+               
+                <div class="col-md-3 text-center tileBox">
+                    <div class="txtHead"><img src="../images/team/index3.jpg" style="width:40%;" />
+                    <h3>Edelito <span class="id-color">Albaracin</span></h3>
+                    <h6>$index[3]</h6></div>
+                    <p>System Analyst</p>
+                </div>
+                <!-- end: -->
+
+                <!-- item -->
+                <div class="col-md-3 text-center tileBox">
+                <br/>
+                    <div class="txtHead"><img src="../images/team/index4.jpg" style="width:40%;" />
+                    <h3>Isidro <span class="id-color">Estose Jr.</span></h3>
+                    <h6>$index[4]</h6></div>
+                    <p>QA Tester</p>
+                </div>
+
+                <!-- item -->
+                <div class="col-md-9 text-center tileBox">
+                <br/>
+                    <div class="txtHead"><img src="../images/team/index5.jpg" style="width:40%;" />
+                    <h3><span class="id-color">$index[5]</span></h3></div>
+                    <p>Team</p>
+                </div>
+                <!-- end: -->
+            </div>
+        </div>
+    </section>
+    <!--Quote-->
+    <section id="quote" class="bg-parlex">
+        <div class="parlex-back">
+            <div class="container secPad text-center">
+				<h2>If I asked people what they wanted, they would have said ‘Faster Horses’.</h2><h3>-Henry Ford</h3>
+            </div>
+            <!--/.container-->
+        </div>
+    </section>
+    
+    <!--Skills
+    <section id="skills" class="secPad white">
+    	<div class="container">
+        <div class="heading text-center">
+                 Heading 
+                <h2>Login</h2>
+                  <div class="col-sm-4">
+                      
+                  </div>
+                  <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="name">Username</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your Username">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <input type="password" class="form-control" name="email" id="email" placeholder="Enter password" title="Please enter your password">
+                        </div>
+                    </div>
+            </div>
+        	<div class="row">
+                <div class="col-sm-12">
+                     
+                </div>
+                <div class="col-sm-6">
+               
+                </div>
+            </div>
+        </div>        
+    </section>-->
+    
+    <!--Experience-->
+    <!--<section id="experience" class="secPad">
+    	<div class="container">     
+           <div class="heading text-center">
+                 Heading 
+                <h2>Register</h2>
+              
+            </div>
+        </div>
+    </section>-->
+   <!--Portfolio-->
+    <section id="portfolio" class="page-section section appear clearfix secPad">
+        <div class="container">
+
+            <div class="heading text-center">
+                <!-- Heading -->
+                <h2>TOP 5 IDEAS</h2>            
+            </div>
+             <div class="col-lg-1 col-xs-4"></div>
+              <div class="col-lg-2 col-xs-5">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                       </br>
+                  <h3>1st</h3>
+           
+              </br>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-rocket"></i>
+                </div>
+                <a href="#" class="small-box-footer" data-toggle="modal" data-target="#topidea">
+                    View Ideas <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+               <div class="col-lg-2 col-xs-5">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                       </br>
+                  <h3>2nd</h3>
+           
+              </br>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-rocket"></i>
+                </div>
+               <a href="#" class="small-box-footer" data-toggle="modal" data-target="#topidea">
+                  View Ideas <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+               <div class="col-lg-2 col-xs-5">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                       </br>
+                  <h3>3rd</h3>
+           
+              </br>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-rocket"></i>
+                </div>
+               <a href="#" class="small-box-footer" data-toggle="modal" data-target="#topidea">
+                   View Ideas <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+               <div class="col-lg-2 col-xs-5">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                       </br>
+                  <h3>4th</h3>
+           
+              </br>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-rocket"></i>
+                </div>
+                <a href="#" class="small-box-footer" data-toggle="modal" data-target="#topidea">
+                  View Ideas <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+               <div class="col-lg-2 col-xs-5">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                       </br>
+                  <h3>5th</h3>
+           
+              </br>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-rocket"></i>
+                </div>
+                <a href="#" class="small-box-footer" data-toggle="modal" data-target="#topidea">
+                  View Ideas <i class="fa fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div><!-- ./col -->
+
+           
+         </div>
+    </section>
+
+	<!--Contact -->
+    <section id="contactUs" class="page-section secPad">
+        <div class="container">
+
+            <div class="row">
+                <div class="heading text-center">
+                    <!-- Heading -->
+                    <h2>Let's Keep In Touch!</h2>
+            
+                </div>
+            </div>
+
+            <div class="row mrgn30">
+
+                <form method="post" action="" id="contactfrm" role="form">
+
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your name (at least 2 characters)">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email" title="Please enter a valid email address">
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="comments">Comments</label>
+                            <textarea name="comment" class="form-control" id="comments" cols="3" rows="5" placeholder="Enter your message…" title="Please enter your message (at least 10 characters)"></textarea>
+                        </div>
+                        <button name="submit" type="submit" class="btn btn-lg btn-primary" id="submit">Submit</button>
+                        <div class="result"></div>
+                    </div>
+                </form>
+                <div class="col-sm-4">
+                    <h4>Address:</h4>
+                    <address>
+                        Index[5] Company<br>
+                       Sangi New Road<br>
+                        Lapu-lapu city
+               
+                        <br>
+                    </address>
+                    <h4>Phone:</h4>
+                    <address>
+                        12345-49589-2<br>
+                    </address>
+                </div>
+            </div>
+        </div>
+        <!--/.container-->
+
+    </section>
+    
+<?php
+  include("footer.php");
+ ?>
