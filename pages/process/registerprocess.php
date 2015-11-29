3e9ac7e0d2081eafa1b718b221b63f70fd81d484
alfiedimpas <alfiedimpas@icloud.com>
2015-11-30 00:59:42 +0800

    wewew

--- pages/process/registerprocess.php
+++ pages/process/registerprocess.php
@@ -15,14 +15,14 @@ include("dbcon.php");
 	      	 $password = md5($_POST['password']);
 	      	 $address = $_POST['address'];
 	      	 $picid=uniqid('pi');
-			 $result = mysql_query("SELECT * FROM User_MD WHERE emailAdd='$email'");
+			 $result = mysql_query("SELECT * FROM user_md WHERE emailAdd='$email'");
 		    	if(mysql_num_rows($result))
 				{	
 					$_SESSION['error']="Email address already registered!";
 					header("location:../index.php");
 				}
 				else{
-			      	 mysql_query("INSERT INTO User_MD(userId,userType,dateRegistered,address,emailAdd,password,profilePicId)
+			      	 mysql_query("INSERT INTO user_md(userId,userType,dateRegistered,address,emailAdd,password,profilePicId)
 			      	 VALUES('$userId','Ideator',NOW(),'$address','$email','$password','$picid')");
 			      	 mysql_query("INSERT INTO user_dtl(userId,lName,fName,midInit,age,gender)
 			      	 VALUES('$userId','$lname','$fname','$midinit','$age','$gender')");
@@ -51,14 +51,14 @@ include("dbcon.php");
 	      	 $password = md5($_POST['password']);
 	      	 $address = $_POST['address'];
 	      	 $picid=uniqid('pi');
-			 $result = mysql_query("SELECT * FROM User_MD WHERE emailAdd='$email'");
+			 $result = mysql_query("SELECT * FROM user_md WHERE emailAdd='$email'");
 		    	if(mysql_num_rows($result))
 				{	
 					$_SESSION['error']="Email address already registered!";
 					header("location:../index.php");
 				}else
 				{
-			      	 mysql_query("INSERT INTO User_MD(userId,userType,dateRegistered,address,emailAdd,password)
+			      	 mysql_query("INSERT INTO user_md(userId,userType,dateRegistered,address,emailAdd,password)
 			      	 VALUES('$userId','Investor',NOW(),'$address','$email','$password')");
 			      	 mysql_query("INSERT INTO user_dtl(userId,lName,fName,midInit,age,gender,profilePic)
 			      	 VALUES('$userId','$lname','$fname','$midinit','$age','$gender','1')");
@@ -86,14 +86,14 @@ include("dbcon.php");
 	      	 $email = $_POST['email'];
 	      	 $password = md5($_POST['password']);
 	      	 $picid=uniqid('pi');
-	    	 $result = mysql_query("SELECT * FROM User_MD WHERE emailAdd='$email'");
+	    	 $result = mysql_query("SELECT * FROM user_md WHERE emailAdd='$email'");
 		    	if(mysql_num_rows($result))
 				{	
 					$_SESSION['error']="Email address already registered!";
 					header("location:../index.php");
 				}else
 				{
-			      	 mysql_query("INSERT INTO User_MD(userId,userType,dateRegistered,address,emailAdd,password)
+			      	 mysql_query("INSERT INTO user_md(userId,userType,dateRegistered,address,emailAdd,password)
 			      	 VALUES('$userId','Company',NOW(),'$address','$email','$password')");
 			      	 mysql_query("INSERT INTO company_dtl(userId,companyName,businessType,ownerName,yearFounded)
 			      	 VALUES('$userId','$companyname','$businesstype','$ownername','$yearfounded')");
